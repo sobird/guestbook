7fd4306947d96747888b268826bbbcd6c0af246d
sobird <i@sobird.me>
2021-11-09 17:23:12 +0800

    动态路由

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
               {id}
               <br />
