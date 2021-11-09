5b957688698583a278f292f55b95b3ff48111fa9
sobird <i@sobird.me>
2021-11-09 16:39:56 +0800

    apply getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,10 @@ import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts'
+
+export default function Home({ allPostsData }) {
+  console.log(`allPostsData`, allPostsData)
   return (
     <Layout home>
       <Head>
@@ -16,6 +19,36 @@ export default function Home() {
           <a href="https://www.nextjs.cn/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
+}
+
+/**
+ * Because it’s meant to be run at build time, 
+ * you won’t be able to use data that’s only available during request time, 
+ * such as query parameters or HTTP headers.
+ * 
+ * @returns 
+ */
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
