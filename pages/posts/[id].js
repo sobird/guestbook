0da5d661af0c8ef6cb73757d9632d0fa85108c04
sobird <i@sobird.me>
2021-11-09 17:38:43 +0800

    Render Markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,18 +1,26 @@
+import Head from 'next/dist/shared/lib/head'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post({ postData }) {
   return <Layout>
-    {postData.title}
-    <br />
-    {postData.id}
-    <br />
-    {postData.date}
+    <Head>
+      <title>{postData.title}</title>
+    </Head>
+    <article>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </article>
   </Layout>
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
