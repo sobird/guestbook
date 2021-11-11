97efb67ff3f289afb5b8d9dbc4843190b5ca3787
sobird <i@sobird.me>
2021-11-11 19:57:34 +0800

    Customizing The 400&500 Page

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+/**
+ * Customizing The 404 Page
+ * This file is statically generated at build time.
+ * 
+ * sobird<i@sobird.me> at 2021/11/11 19:52:11 created.
+ */
+
+
+export default function Custom404() {
+  return <h1>404 - Page Not Found</h1>
+}
---
+++ pages/500.tsx
@@ -0,0 +1,9 @@
+/**
+ * Customizing The 500 Page
+ *
+ * sobird<i@sobird.me> at 2021/11/11 19:55:33 created.
+ */
+
+export default function Custom500() {
+  return <h1>500 - Server-side error occurred</h1>;
+}
