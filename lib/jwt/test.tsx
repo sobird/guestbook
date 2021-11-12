daa9a05a92f718ad741b29c68813971d581f7160
sobird <i@sobird.me>
2021-11-12 14:27:07 +0800

    fix:upgrade next version https://github.com/vercel/next.js/issues/30911

---
+++ lib/jwt/test.tsx
@@ -0,0 +1,15 @@
+const {SignJWT} = require('jose');
+
+
+const test = async () => {
+  const token = await new SignJWT({})
+      .setProtectedHeader({ alg: 'HS256' })
+      .setJti('1212')
+      .setIssuedAt()
+      .setExpirationTime('2h')
+      .sign(new TextEncoder().encode('112'))
+
+  console.log(token)
+}
+
+test();
