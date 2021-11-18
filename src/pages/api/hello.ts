f8926edfe1510b55b46a9874002d22cfcfc2c5bb
sobird <i@sobird.me>
2021-11-18 10:45:03 +0800

    restful

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -3,6 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { compose, Next } from "@/lib/middleware";
 
 import { User } from "@/models";
+import rest from "@/lib/rest";
 
 // 如果表不存在,则创建该表(如果已经存在,则不执行任何操作)
 // User.sync({ force: true });
@@ -15,6 +16,4 @@ const mid = function (req: NextApiRequest, res: NextApiResponse, next: Next) {
   next();
 };
 
-export default compose(mid, (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ text: "Hello" });
-});
+export default compose(mid, rest.bind(module.exports));
