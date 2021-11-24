25746c8491c116a92c18ee2fa25166556cb3c354
sobird <i@sobird.me>
2021-11-24 13:11:52 +0800

    移除antd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import { GetServerSidePropsContext } from "next";
 import Link from "next/link";
 import useSWR from "swr";
-
-import { Form, Input, Button, Checkbox } from "antd";
-import { Button as MButton, TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
 import Layout from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
@@ -21,58 +21,90 @@ export default function Home({}) {
 
   return (
     <Layout>
-      <MButton variant="contained" size="small">按钮</MButton>
+      <Button variant="contained" size="small">
+        按钮
+      </Button>
       <TextField variant="outlined" label="用户" size="small" />
-      <section className={utilStyles.headingMd}>
-        <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          <Form.Item
-            label="用户名称"
-            name="username"
-            rules={[{ required: true, message: "Please input your username!" }]}
+      <section className={utilStyles.headingMd}></section>
+      <Grid container spacing={1}>
+        <Grid item xs={12} sm={4}>
+          <Box
+            sx={{
+              bgcolor: "primary.main",
+              color: "primary.contrastText",
+              p: 2,
+            }}
           >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="用户邮箱"
-            name="email"
-            rules={[{ required: true, message: "Please input your email!" }]}
+            primary.main
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Box
+            sx={{
+              bgcolor: "secondary.main",
+              color: "secondary.contrastText",
+              p: 2,
+            }}
           >
-            <Input type="email" />
-          </Form.Item>
-
-          <Form.Item
-            label="登录密码"
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
+            secondary.main
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Box
+            sx={{ bgcolor: "error.main", color: "error.contrastText", p: 2 }}
           >
-            <Input.TextArea />
-          </Form.Item>
-
-          <Form.Item
-            name="remember"
-            valuePropName="checked"
-            wrapperCol={{ offset: 8, span: 16 }}
+            error.main
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Box
+            sx={{
+              bgcolor: "warning.main",
+              color: "warning.contrastText",
+              p: 2,
+            }}
           >
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </section>
-      <section></section>
+            warning.main
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Box sx={{ bgcolor: "info.main", color: "info.contrastText", p: 2 }}>
+            info.main
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Box
+            sx={{
+              bgcolor: "success.main",
+              color: "success.contrastText",
+              p: 2,
+            }}
+          >
+            success.main
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Box
+            sx={{ bgcolor: "text.primary", color: "background.paper", p: 2 }}
+          >
+            text.primary
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Box
+            sx={{ bgcolor: "text.secondary", color: "background.paper", p: 2 }}
+          >
+            text.secondary
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Box
+            sx={{ bgcolor: "text.disabled", color: "background.paper", p: 2 }}
+          >
+            text.disabled
+          </Box>
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
