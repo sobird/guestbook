ad095138617670d80c70b8da5ec6b52f1616550d
sobird <i@sobird.me>
2023-10-31 18:15:31 +0800

    upate

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
 /**
  * next.config.js
  * 
+ * @see https://nextjs.org/docs/pages/api-reference/next-config-js
+ * 
  * sobird<i@sobird.me> at 2021/11/15 15:11:17 created.
  */
 
 const { resolve } = require('path');
 
 module.exports = {
+  output: 'standalone',
+
+  // Use the CDN in production and localhost for development.
+  // assetPrefix: isProd ? 'https://cdn.mydomain.com' : undefined,
+  // an empty string, the default
+  // basePath: '/docs',
+  // 默认值 .next
+  // distDir: '.next',
+
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.alias['@'] = resolve(__dirname, 'src');
     // Important: return the modified config
